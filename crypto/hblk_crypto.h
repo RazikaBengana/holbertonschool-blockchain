91a2c272c1f04030459ff2e87eb289a9f75300a6
RazikaBengana <razikabengana@gmail.com>
2024-04-25 23:13:49 +0200

    Header file

---
+++ crypto/hblk_crypto.h
@@ -0,0 +1,77 @@
+#ifndef HBLK_CRYPTO_H
+#define HBLK_CRYPTO_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/pem.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+
+
+# define EC_CURVE   NID_secp256k1
+
+/* EC_KEY public key octet string length (using 256-bit curve) */
+# define EC_PUB_LEN 65
+
+/* Maximum signature octet string length (using 256-bit curve) */
+# define SIG_MAX_LEN    72
+
+# define PRI_FILENAME   "key.pem"
+# define PUB_FILENAME   "key_pub.pem"
+
+
+
+/**
+ * struct sig_s - EC Signature structure
+ *
+ * @sig: Signature buffer;
+ *       the whole space may not be used
+ * @len: Actual signature size;
+ *       can't exceed SIG_MAX_LEN, therefore stored on a byte
+ */
+
+typedef struct sig_s
+{
+	uint8_t     sig[SIG_MAX_LEN];
+	uint8_t     len;
+} sig_t;
+
+
+
+/* task 0 */
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH]);
+
+/* task 1 */
+EC_KEY *ec_create(void);
+
+/* task 2 */
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+
+/* task 3 */
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+
+/* task 4 */
+int ec_save(EC_KEY *key, char const *folder);
+
+/* task 5 */
+EC_KEY *ec_load(char const *folder);
+
+/* task 6 */
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+		 size_t msglen, sig_t *sig);
+
+/* task 7 */
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+	      size_t msglen, sig_t const *sig);
+
+
+
+#endif /* HBLK_CRYPTO_H */
