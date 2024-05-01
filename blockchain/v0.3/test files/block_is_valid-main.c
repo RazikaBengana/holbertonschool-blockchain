3325c335244733213a4ca95568712f1a13911de9
RazikaBengana <razikabengana@gmail.com>
2024-05-01 16:42:37 +0200

    Revert `block_is_valid-main.c` to its original state in `v0.3/test files`

--- blockchain/v0.3/test files/block_is_valid-main.c
+++ blockchain/v0.3/test files/block_is_valid-main.c
@@ -1,60 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 #include "../blockchain.h"
-
-void _blockchain_print_brief(blockchain_t const *blockchain);
-void _print_hex_buffer(uint8_t const *buf, size_t len);
-
-/**
- * _add_block - Adds a new block to the blockchain with specified data and mines it
- *
- * this function creates a new block with the specified data
- * and a coinbase transaction created for the miner;
- * it then attempts to mine the block;
- * if the block is valid, it is added to the blockchain;
- * otherwise, an error is logged
- *
- * @blockchain: Pointer to the blockchain to which the block will be added
- * @prev: Pointer to the previous block in the blockchain
- * @data: Data to be stored in the new block
- * @miner: EC_KEY pointer representing the miner's public key
- *
- * Return: Pointer to the newly added block
- */
-
-static block_t *_add_block(blockchain_t *blockchain, block_t const *prev,
-			   char const *data, EC_KEY *miner)
-{
-	block_t *block;
-	transaction_t *coinbase;
-
-	block = block_create(prev, (int8_t *)data, (uint32_t)strlen(data));
-	block->info.difficulty = 20;
-
-	coinbase = coinbase_create(miner, block->info.index);
-	llist_add_node(block->transactions, coinbase, ADD_NODE_FRONT);
-
-	block_mine(block);
-
-	if (block_is_valid(block, prev, blockchain->unspent) == 0)
-	{
-		printf("Block mined: [%u] ", block->info.difficulty);
-		_print_hex_buffer(block->hash, SHA256_DIGEST_LENGTH);
-		printf("\n");
-		llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-	}
-	else
-	{
-		fprintf(stderr, "Invalid Block with index: %u\n",
-			block->info.index);
-	}
-
-	return (block);
-}
-
-
 
 /**
  * main - Entry point
@@ -65,18 +12,24 @@ static block_t *_add_block(blockchain_t *blockchain, block_t const *prev,
 int main(void)
 {
 	blockchain_t *blockchain;
-	block_t *block;
-	EC_KEY *miner;
+	block_t *first, *block;
 
-	miner = ec_create();
 	blockchain = blockchain_create();
+	first = llist_get_head(blockchain->chain);
 
-	block = llist_get_head(blockchain->chain);
-	block = _add_block(blockchain, block, "Holberton", miner);
-	block = _add_block(blockchain, block, "School", miner);
+	block = block_create(first, (int8_t *)"Holberton", 9);
+	block_hash(block, block->hash);
+	llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+
+	if (block_is_valid(first, NULL) != 0 ||
+	    block_is_valid(block, first) != 0)
+	{
+		fprintf(stderr, "Block invalid\n");
+		return (EXIT_FAILURE);
+	}
+	printf("Block is valid\n");
 
 	blockchain_destroy(blockchain);
-	EC_KEY_free(miner);
 
 	return (EXIT_SUCCESS);
 }
